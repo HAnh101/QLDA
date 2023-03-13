5e232dccc838413c0273c6377a9596d233362568
MaiManh15 <sorryfornoob15@gmail.com>
2023-03-13 15:56:43 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,12 @@
 print('bello')
 #ok
 <<<<<<< HEAD
+<<<<<<< HEAD
 # troi oi
 # lam sai roi
 =======
 #hehe 
 print('blah blah')
 >>>>>>> 112be40e66336dc546a009ee0909e651040bcc73
+=======
+>>>>>>> parent of ba10fbb (Update a.py)
